f693ead9cc0cbee55111a52fc8a2dcf63012d498
viralsir <viralsir2018@gmail.com>
2020-10-20 12:35:39 +0530

    i have added three no checking functionality .

--- FIRSTDEMO.py
+++ FIRSTDEMO.py
@@ -1,8 +1,10 @@
 no1=int(input("Enter No1:"))
 no2=int(input("Enter No2:"))
+no3=int(input("Enter No3:"))
 
-if no1>no2:
+if no1>no2 and no1>no3:
     print(no1," is a maximum no")
-else:
+elif no2>no3:
     print(no2," is a maximum no")
-
+else :
+    print(no3," is a maximum no")
