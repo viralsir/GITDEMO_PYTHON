9d3ff26c6830132bbec88bc2761686f66ed94ecc
viralsir <53177289+viralsir@users.noreply.github.com>
2020-10-20 12:50:21 +0530

    I have added  functionality checking no positive or not

--- FIRSTDEMO.py
+++ FIRSTDEMO.py
@@ -3,9 +3,12 @@ no1=int(getvalue("No1"))
 no2=int(getvalue("No2"))
 no3=int(getvalue("No3"))
 
-if no1>no2 and no1>no3:
-    print(no1," is a maximum no")
-elif no2>no3:
-    print(no2," is a maximum no")
+if no1>0 and no2>0 and no3>0 :
+    if no1>no2 and no1>no3:
+        print(no1," is a maximum no")
+    elif no2>no3:
+        print(no2," is a maximum no")
+    else :
+        print(no3," is a maximum no")
 else :
-    print(no3," is a maximum no")
+    print("invalid input ")
